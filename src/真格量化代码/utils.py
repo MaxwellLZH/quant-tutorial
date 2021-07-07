f90018b3350bcdb14e520a90a1021dced237724a
Max Liu <zl2480@columbia.edu>
2021-07-07 23:06:06 +0800

    update

--- src/真格量化代码/utils.py
+++ src/真格量化代码/utils.py
@@ -2,18 +2,27 @@
 from PoboAPI import *
 
 
-# 获得距离期货期权到期日的自然日天数
 def get_days_to_expire(contract):
+    """获得距离期货期权到期日的自然日天数
+    :param contract: str, 合约代码
+    """
     info = GetContractInfo(contract)
     due_date = info.get("行权到期日", info["最后交易日"])
     cur_time = GetCurrentTime()
     return (due_date - cur_time.date()).days
 
 
-# 获得距离期货期权到期日的交易日天数
 def get_trading_days_to_expire(contract, exchange="SHFE"):
+    """获得距离期货期权到期日的交易日天数
+    :param contract: str, 合约代码
+    """
     info = GetContractInfo(contract)
     due_date = info.get("行权到期日", info["最后交易日"])
     tds = GetTradingDates(exchange, GetCurrentTime(), due_date)
     # 返回的交易日列表是包含两端日期的
     return len(tds) - 1 if tds[-1] == due_date else len(tds)
+
+
+def get_actively_trading_merchant_codes(exchange="SHFE", lookback=90, threshold=1e8):
+    """获取指定交易所下活跃的期货品种"""
+    pass
