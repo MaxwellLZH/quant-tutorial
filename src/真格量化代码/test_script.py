8b29636adea96c271980adcce1ed90a54da041db
Max Liu <zl2480@columbia.edu>
2021-07-07 23:18:20 +0800

    convert to numpydoc format commentting

---
+++ src/真格量化代码/test_script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding:utf-8
+# EmuCounter2
+# 用于在真格平台上测试代码的脚本
+
+from PoboAPI import *
+
+
+def OnStart(context) :
+	# 这里测试只用一次的代码
+    pass
--- src/真格量化代码/utils.py
+++ src/真格量化代码/utils.py
@@ -3,8 +3,18 @@ from PoboAPI import *
 
 
 def get_days_to_expire(contract):
-    """获得距离期货期权到期日的自然日天数
-    :param contract: str, 合约代码
+    """
+    获得距离期货期权到期日的自然日天数
+    
+    Parameters
+    ----------
+    contract : str
+        合约代码
+
+    Returns
+    -------
+    n_days : int
+        距离到期日的自然日天数
     """
     info = GetContractInfo(contract)
     due_date = info.get("行权到期日", info["最后交易日"])
@@ -12,9 +22,20 @@ def get_days_to_expire(contract):
     return (due_date - cur_time.date()).days
 
 
+
 def get_trading_days_to_expire(contract, exchange="SHFE"):
-    """获得距离期货期权到期日的交易日天数
-    :param contract: str, 合约代码
+    """
+    获得距离期货期权到期日的交易日天数
+    
+    Parameters
+    ----------
+    contract : str
+        合约代码
+
+    Returns
+    -------
+    n_days : int
+        距离到期日的交易日天数
     """
     info = GetContractInfo(contract)
     due_date = info.get("行权到期日", info["最后交易日"])
@@ -23,6 +44,20 @@ def get_trading_days_to_expire(contract, exchange="SHFE"):
     return len(tds) - 1 if tds[-1] == due_date else len(tds)
 
 
+
+def add_days(n, curr=None):
+    pass
+
+
+def add_trading_days(n, curr=None):
+    """获得curr时点加上n个交易日的日期
+    :param n: int, 
+    :param n: 
+    """
+
+
 def get_actively_trading_merchant_codes(exchange="SHFE", lookback=90, threshold=1e8):
     """获取指定交易所下活跃的期货品种"""
-    pass
+    from 
+
+    merchant_codes = GetVarieties(exchange)
