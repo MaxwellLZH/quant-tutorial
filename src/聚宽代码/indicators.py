746d34b539b049fc3e526746c7989b42f3c4c6b9
Maxwell <zl2480@columbia.edu>
2023-03-13 22:09:41 +0800

    Update indicators.py

--- src/聚宽代码/indicators.py
+++ src/聚宽代码/indicators.py
@@ -14,9 +14,22 @@ class Indicators:
         self.unit = unit
 
     def get_price_history(self, count, unit=None, return_field=None):
-        uint = unit or self.unit
-        df = attribute_history(self.dom, count=count, unit=self.uint, fields=['open','close','high','low','volume','money'], skip_paused=True)
+        unit = unit or self.unit
+        df = attribute_history(self.dom, count=count, unit=unit, fields=['open','close','high','low','volume','money'], skip_paused=True)
         if return_field is not None:
             return df[return_field]
 
-    
+    def ma(self, window: int, ma_type: int = 1):
+        """ 
+        {0: 'Simple Moving Average',
+        1: 'Exponential Moving Average',
+        2: 'Weighted Moving Average',
+        3: 'Double Exponential Moving Average',
+        4: 'Triple Exponential Moving Average',
+        5: 'Triangular Moving Average',
+        6: 'Kaufman Adaptive Moving Average',
+        7: 'MESA Adaptive Moving Average',
+        8: 'Triple Generalized Double Exponential Moving Average'}
+        """
+        df_close = self.get_price_history(count=window+10, return_field="close")
+        return talib.MA(df_close, window, matype=ma_type).values[-1]
